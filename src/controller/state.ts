c79045d46497624234d07eb0a528cc31eda01514
Fabrizio Panato <fbzpanatto@hotmail.com>
2023-10-02 14:29:48 -0300

    in progress...

--- src/controller/state.ts
+++ src/controller/state.ts
@@ -1,23 +1,10 @@
 import { GenericController } from "./genericController";
-import {EntityTarget, FindManyOptions, ObjectLiteral} from "typeorm";
+import { EntityTarget } from "typeorm";
 import { State } from "../model/State";
 
 class StateController extends GenericController<EntityTarget<State>> {
   constructor() {
     super(State);
-  }
-
-  override async findAllWhere(options: FindManyOptions<ObjectLiteral> | undefined) {
-    try {
-      const condition = true
-
-      const result = await this.repository.find(options);
-
-      if( condition ) {
-        return { status: 404, message: 'Data not found' };
-      }
-      return { status: 200, data: result };
-    } catch (error: any) { return { status: 500, message: error.message } }
   }
 }
 

cb42294fdd2502070a78ceb9c7b18bfa15af8b86
Fabrizio Panato <fbzpanatto@hotmail.com>
2023-11-08 14:34:58 -0300

    in progress...

--- src/controller/personCategory.ts
+++ src/controller/personCategory.ts
@@ -1,12 +1,31 @@
 import { GenericController } from "./genericController";
-import { EntityTarget } from "typeorm";
+import {EntityTarget, FindManyOptions, ObjectLiteral} from "typeorm";
 import { PersonCategory } from "../model/PersonCategory";
+import {Request} from "express";
+import {AppDataSource} from "../data-source";
+import {personCategories} from "../utils/personCategories";
 
 class PersonCategoryController extends GenericController<EntityTarget<PersonCategory>> {
 
   constructor() {
     super(PersonCategory);
   }
+
+  override async findAllWhere(options: FindManyOptions<ObjectLiteral> | undefined, request?: Request) {
+
+    const studentCategoryId = personCategories.ALUNO
+
+    try {
+      const result = await AppDataSource.getRepository(PersonCategory)
+        .createQueryBuilder('personCategory')
+        .where('personCategory.id != :id', { id: studentCategoryId })
+        .getMany();
+
+      return { status: 200, data: result };
+    } catch (error: any) { return { status: 500, message: error.message } }
+  }
+
+
 }
 
 export const personCategoryController = new PersonCategoryController();

acd1786cf716b9872acd2ecb9b7a910110bf452c
Fabrizio Panato <fbzpanatto@hotmail.com>
2023-11-08 14:28:22 -0300

    in progress...

--- src/controller/teacher.ts
+++ src/controller/teacher.ts
@@ -117,6 +117,43 @@ class TeacherController extends GenericController<EntityTarget<Teacher>> {
 
   override async save(body: TeacherBody, options: SaveOptions | undefined) {
     try {
+
+      const user = await this.teacherByUser(body.user.user) as Teacher
+      if(user.person.category.id === personCategories.MONITOR_DE_INFORMATICA) {
+        const canCreate = [personCategories.PROFESSOR, personCategories.MONITOR_DE_INFORMATICA, personCategories.ALUNO]
+        if(!canCreate.includes(body.category.id)) { return { status: 401, message: 'Você não tem permissão para criar este registro.' } }
+      }
+
+      if(user.person.category.id === personCategories.SECRETARIO) {
+        const canCreate = [personCategories.PROFESSOR, personCategories.MONITOR_DE_INFORMATICA, personCategories.ALUNO, personCategories.SECRETARIO]
+        if(!canCreate.includes(body.category.id)) { return { status: 401, message: 'Você não tem permissão para criar este registro.' } }
+      }
+
+      if(user.person.category.id === personCategories.COORDENADOR) {
+        const canCreate = [personCategories.PROFESSOR, personCategories.MONITOR_DE_INFORMATICA, personCategories.ALUNO, personCategories.SECRETARIO, personCategories.COORDENADOR]
+        if(!canCreate.includes(body.category.id)) { return { status: 401, message: 'Você não tem permissão para criar este registro.' } }
+      }
+
+      if(user.person.category.id === personCategories.VICE_DIRETOR) {
+        const canCreate = [personCategories.PROFESSOR, personCategories.MONITOR_DE_INFORMATICA, personCategories.ALUNO, personCategories.SECRETARIO, personCategories.COORDENADOR, personCategories.VICE_DIRETOR]
+        if(!canCreate.includes(body.category.id)) { return { status: 401, message: 'Você não tem permissão para criar este registro.' } }
+      }
+
+      if(user.person.category.id === personCategories.DIRETOR) {
+        const canCreate = [personCategories.PROFESSOR, personCategories.MONITOR_DE_INFORMATICA, personCategories.ALUNO, personCategories.SECRETARIO, personCategories.COORDENADOR, personCategories.VICE_DIRETOR, personCategories.DIRETOR]
+        if(!canCreate.includes(body.category.id)) { return { status: 401, message: 'Você não tem permissão para criar este registro.' } }
+      }
+
+      if(user.person.category.id === personCategories.SUPERVISOR) {
+        const canCreate = [personCategories.PROFESSOR, personCategories.MONITOR_DE_INFORMATICA, personCategories.ALUNO, personCategories.SECRETARIO, personCategories.COORDENADOR, personCategories.VICE_DIRETOR, personCategories.DIRETOR, personCategories.SUPERVISOR]
+        if(!canCreate.includes(body.category.id)) { return { status: 401, message: 'Você não tem permissão para criar este registro.' } }
+      }
+
+      if(user.person.category.id === personCategories.ADMINISTRADOR) {
+        const canCreate = [personCategories.PROFESSOR, personCategories.MONITOR_DE_INFORMATICA, personCategories.ALUNO, personCategories.SECRETARIO, personCategories.COORDENADOR, personCategories.VICE_DIRETOR, personCategories.DIRETOR, personCategories.SUPERVISOR, personCategories.ADMINISTRADOR]
+        if(!canCreate.includes(body.category.id)) { return { status: 401, message: 'Você não tem permissão para criar este registro.' } }
+      }
+
       const category = await AppDataSource.getRepository(PersonCategory).findOne({ where: { id: body.category.id } } ) as PersonCategory
       const person = this.createPerson({ name: body.name, birth: body.birth, category })
       const teacher = await this.repository.save(this.createTeacher(person))

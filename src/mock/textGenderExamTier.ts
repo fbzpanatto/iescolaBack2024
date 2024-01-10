a52aa2e8e9f7e4040352074fce4106d0dbbbc66f
Fabrizio Panato <fbzpanatto@hotmail.com>
2024-01-10 11:27:42 -0300

    in progress...

--- src/mock/textGenderExamTier.ts
+++ src/mock/textGenderExamTier.ts
@@ -1,4 +1,4 @@
 export const TEXTGENDEREXAMTIER = [
-  { name: "PERÍODO INICIAL", color: "#6696b4" },
-  { name: "PERÍODO FINAL", color: "#8cb466" },
+  { name: "PERÍODO INICIAL", color: "#3989bb" },
+  { name: "PERÍODO FINAL", color: "#74b635" },
 ];

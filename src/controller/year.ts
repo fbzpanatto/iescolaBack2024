495efc58c62d0b5441c9a793a1add889654c9ad2
Fabrizio Panato <fbzpanatto@hotmail.com>
2024-01-30 15:14:23 -0300

    staging

--- src/controller/year.ts
+++ src/controller/year.ts
@@ -1,12 +1,12 @@
-import {GenericController} from "./genericController";
-import {EntityTarget, FindManyOptions, ILike, IsNull, ObjectLiteral, SaveOptions} from "typeorm";
-import {Year} from "../model/Year";
-import {Bimester} from "../model/Bimester";
-import {Period} from "../model/Period";
-import {AppDataSource} from "../data-source";
-import {Request} from "express";
-import {personCategories} from "../utils/personCategories";
-import {StudentClassroom} from "../model/StudentClassroom";
+import { GenericController } from "./genericController";
+import { EntityTarget, FindManyOptions, ILike, IsNull, ObjectLiteral, SaveOptions } from "typeorm";
+import { Year } from "../model/Year";
+import { Bimester } from "../model/Bimester";
+import { Period } from "../model/Period";
+import { AppDataSource } from "../data-source";
+import { Request } from "express";
+import { personCategories } from "../utils/personCategories";
+import { StudentClassroom } from "../model/StudentClassroom";
 
 class YearController extends GenericController<EntityTarget<Year>> {
   constructor() { super(Year) }
@@ -34,7 +34,7 @@ class YearController extends GenericController<EntityTarget<Year>> {
       const teacher = await this.teacherByUser(body.user.user)
       const canCreate = [personCategories.ADMINISTRADOR]
 
-      if(!canCreate.includes(teacher.person.category.id)) {
+      if (!canCreate.includes(teacher.person.category.id)) {
         return { status: 403, message: 'Você não tem permissão para criar um ano letivo. Solicite a um Administrador do sistema.' }
       }
 
@@ -42,7 +42,7 @@ class YearController extends GenericController<EntityTarget<Year>> {
       if (yearExists && yearExists.name === body.name) { return { status: 404, message: `O ano ${body.name} já existe.` } }
 
       const currentYear = await this.currentYear() as Year
-      if(currentYear && currentYear.active && body.active) { return { status: 404, message: `O ano ${currentYear.name} está ativo. Encerre-o antes de criar um novo.` } }
+      if (currentYear && currentYear.active && body.active) { return { status: 404, message: `O ano ${currentYear.name} está ativo. Encerre-o antes de criar um novo.` } }
 
       const baseYear = await AppDataSource.getRepository(Year)
         .createQueryBuilder('year')
@@ -59,9 +59,9 @@ class YearController extends GenericController<EntityTarget<Year>> {
 
       const bimesters = await AppDataSource.getRepository(Bimester).find() as Bimester[]
 
-      for(let bimester of bimesters) {
+      for (let bimester of bimesters) {
         const period = new Period();
-        period.year = newYear  as Year;
+        period.year = newYear as Year;
         period.bimester = bimester;
 
         await AppDataSource.getRepository(Period).save(period)
@@ -75,22 +75,22 @@ class YearController extends GenericController<EntityTarget<Year>> {
   async updateId(id: any, body: any) {
     try {
 
-      const { data} = await this.findOneById(id, {});
+      const { data } = await this.findOneById(id, {});
       const yearToUpdate = data
 
       if (!yearToUpdate) { return { status: 404, message: 'Data not found' } }
 
       const yearExists = await this.checkIfExists(body)
-      if(yearExists && yearExists.name === body.name && yearExists.id !== yearToUpdate.id) { return { status: 400, message: `O ano ${body.name} já existe.` } }
+      if (yearExists && yearExists.name === body.name && yearExists.id !== yearToUpdate.id) { return { status: 400, message: `O ano ${body.name} já existe.` } }
 
       const currentYear = await this.currentYear() as Year
-      if(currentYear && currentYear.active && body.active) { return { status: 400, message: `O ano ${currentYear.name} está ativo.` } }
+      if (currentYear && currentYear.active && body.active) { return { status: 400, message: `O ano ${currentYear.name} está ativo.` } }
 
       for (const prop in body) { yearToUpdate[prop] = body[prop as keyof Year] }
 
-      if(!body.active && body.endedAt === '' || body.endedAt === null) { return { status: 400, message: 'Data de encerramento não pode ser vazia.'} }
+      if (!body.active && body.endedAt === '' || body.endedAt === null) { return { status: 400, message: 'Data de encerramento não pode ser vazia.' } }
 
-      if(!body.active && body.endedAt) {
+      if (!body.active && body.endedAt) {
 
         const allStudentsClassroomsYear = await AppDataSource.getRepository(StudentClassroom)
           .createQueryBuilder('studentClassroom')
@@ -99,8 +99,8 @@ class YearController extends GenericController<EntityTarget<Year>> {
           .andWhere('studentClassroom.endedAt IS NULL')
           .getMany()
 
-        for(let register of allStudentsClassroomsYear) {
-          await AppDataSource.getRepository(StudentClassroom).save({ ...register, endedAt: new Date()})
+        for (let register of allStudentsClassroomsYear) {
+          await AppDataSource.getRepository(StudentClassroom).save({ ...register, endedAt: new Date() })
         }
       }
 
@@ -117,7 +117,7 @@ class YearController extends GenericController<EntityTarget<Year>> {
   }
 
   async checkIfExists(body: Year) {
-    const result = await this.findOneByWhere({ where: { name: body.name }})
+    const result = await this.findOneByWhere({ where: { name: body.name } })
     return result.data as Year;
   }
 }

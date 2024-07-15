3d7dcd83e276b897daab7c58af15c757a0bf5539
Fabrizio Panato <fbzpanatto@hotmail.com>
2024-07-15 09:14:08 -0300

    Build

--- public/data-source.js
+++ public/data-source.js
@@ -5,8 +5,8 @@ const typeorm_1 = require("typeorm");
 const model_1 = require("./model");
 exports.AppDataSource = new typeorm_1.DataSource({
     type: "mysql",
-    // host: "localhost",
-    host: "iescoladb2024.cts1jr1mgndt.us-east-1.rds.amazonaws.com",
+    host: "localhost",
+    // host: "iescoladb2024.cts1jr1mgndt.us-east-1.rds.amazonaws.com",
     port: 3306,
     username: "fbzpanatto",
     password: "fnp181292",
--- src/data-source.ts
+++ src/data-source.ts
@@ -3,8 +3,8 @@ import { entities } from "./model";
 
 export const AppDataSource = new DataSource({
   type: "mysql",
-  // host: "localhost",
-  host: "iescoladb2024.cts1jr1mgndt.us-east-1.rds.amazonaws.com",
+  host: "localhost",
+  // host: "iescoladb2024.cts1jr1mgndt.us-east-1.rds.amazonaws.com",
   port: 3306,
   username: "fbzpanatto",
   password: "fnp181292",

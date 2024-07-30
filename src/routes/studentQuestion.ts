5251e6ce0b05fa0b4e4e3beaf4402498f9beea8c
Fabrizio Panato <fbzpanatto@hotmail.com>
2024-07-30 14:16:06 -0300

    YEAR_NAME_PARAM studentQuestion

--- src/routes/studentQuestion.ts
+++ src/routes/studentQuestion.ts
@@ -1,6 +1,6 @@
 import { Request, Response, Router } from "express";
 import { stuQuestCtrl as controller } from "../controller/StudentQuestion";
-import { ID_PARAM, VALIDATE_STUDENT_QUESTIONSTATUS, BODY_VALIDATION_STUDENT_QUESTION, VALIDATE_STUDENT_QUESTIONANSWER, BODY_VALIDATION_STUDENT_ANSWER} from "../middleware/validators";
+import { ID_PARAM, VALIDATE_STUDENT_QUESTIONSTATUS, BODY_VALIDATION_STUDENT_QUESTION, VALIDATE_STUDENT_QUESTIONANSWER, BODY_VALIDATION_STUDENT_ANSWER, YEAR_NAME_PARAM } from "../middleware/validators";
 import havePermission from "../middleware/havePermission";
 
 const UPDATE_VALIDATORS_ANSWER = [ID_PARAM, VALIDATE_STUDENT_QUESTIONANSWER, BODY_VALIDATION_STUDENT_ANSWER]
@@ -8,7 +8,7 @@ const UPDATE_VALIDATORS_STATUS = [ID_PARAM, VALIDATE_STUDENT_QUESTIONSTATUS, BOD
 
 export const StudentQuestionRouter = Router();
 
-StudentQuestionRouter.put('/:id/question', ...UPDATE_VALIDATORS_ANSWER, havePermission, async (req: Request, res: Response) => {
+StudentQuestionRouter.put('/:id/question', YEAR_NAME_PARAM, ...UPDATE_VALIDATORS_ANSWER, havePermission, async (req: Request, res: Response) => {
   const response = await controller.updateQuestion(req, req.body); return res.status(response.status).json(response)
 });
 

d993f3a73d371adc6d8545411a783ffd0b710555
Fabrizio Panato <fbzpanatto@hotmail.com>
2024-08-08 10:41:23 -0300

    READINGFLUENCYEXAM AND READINGFLUENCYEXAM

---
+++ src/mock/readingFluencyExam.ts
@@ -0,0 +1,7 @@
+export const READINGFLUENCYEXAM = [
+  { name: "PALAVRAS", color: "#58b1d7" },
+  { name: "FRASES", color: "#d79c58" },
+  { name: "TEXTO", color: "#9ad758" },
+  { name: "CLASSIFICAÇÃO GERAL", color: "#9558d7" },
+  { name: "QUESTÕES DE COMPREENSÃO", color: "#5871d7" }
+]

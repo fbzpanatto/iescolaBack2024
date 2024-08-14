1167bf9d40a820b723eba3ed041a8add883d899f
Fabrizio Panato <fbzpanatto@hotmail.com>
2024-08-14 11:17:11 -0300

    staging

--- src/model/QuestionGroup.ts
+++ src/model/QuestionGroup.ts
@@ -1,7 +1,5 @@
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
-import { Length } from "class-validator";
-import { Period } from "./Period";
-import {TestQuestion} from "./TestQuestion";
+import { TestQuestion } from "./TestQuestion";
 
 @Entity()
 export class QuestionGroup {

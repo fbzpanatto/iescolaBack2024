388bea0aef7f810bc8a50b727921b45f11db84d2
Fabrizio Panato <fbzpanatto@hotmail.com>
2024-09-27 10:42:38 -0300

    Student Searching

--- src/controller/student.ts
+++ src/controller/student.ts
@@ -575,7 +575,7 @@ class StudentController extends GenericController<EntityTarget<Student>> {
           qb.where("person.name LIKE :search", { search: `%${options.search}%` })
             .orWhere("student.ra LIKE :search", { search: `%${options.search}%` })
             .orWhere("classroom.shortName LIKE :search", { search: `%${options.search}%` })
-            .orWhere("school.shortName LIKE :search", { search: `%${options.search}%` })
+            .orWhere("school.name LIKE :search", { search: `%${options.search}%` })
         }))
         .andWhere( new Brackets((qb) => {
           if (!masterUser) { qb.andWhere(isOwner ? "classroom.id IN (:...classrooms)" : "classroom.id NOT IN (:...classrooms)", { classrooms: options.teacherClasses?.classrooms } ) } else { qb.andWhere( isOwner ? "classroom.id IN (:...classrooms)" : "classroom.id NOT IN (:...classrooms)", { classrooms: allClassrooms.map((classroom) => classroom.id)})}

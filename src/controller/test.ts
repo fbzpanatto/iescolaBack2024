14783bfd5b6e43115d3c073504729e82efa9ac2d
Fabrizio Panato <fbzpanatto@hotmail.com>
2024-09-27 14:45:21 -0300

    totalOfScPercentage fixes

--- src/controller/test.ts
+++ src/controller/test.ts
@@ -341,7 +341,12 @@ class TestController extends GenericController<EntityTarget<Test>> {
 
                 const studentTotals = { rowTotal: 0, rowPercent: 0 }
                 if(sc.student.studentQuestions.every(sq => sq.answer.length < 1)) { return { ...sc, student: { ...sc.student, studentTotals: { rowTotal: '-', rowPercent: '-' } } } }
-                if(sc.student.studentQuestions.every(sq => sq.rClassroom?.id != classroom.id)) { diffOe += 1; return { ...sc, student: { ...sc.student, studentTotals: { rowTotal: 'OE', rowPercent: 'OE' } } } }
+                if(sc.student.studentQuestions.every(sq => sq.rClassroom?.id != classroom.id)) {
+
+                  sc.student.studentQuestions = sc.student.studentQuestions.map(sq => ({...sq, answer: 'TR'}))
+
+                  diffOe += 1; return { ...sc, student: { ...sc.student, studentTotals: { rowTotal: 'TR', rowPercent: 'TR' } } }
+                }
 
                 validSc += 1
                 validStudentsTotalizator += 1

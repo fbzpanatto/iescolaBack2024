c82dc681eacc2a1e72d916874076ce6ee3691a89
Fabrizio Panato <fbzpanatto@hotmail.com>
2024-11-26 15:06:02 -0300

    BIG CHANGES TYPEORM QUERIES TO PURE SQL QUERIES...

--- src/controller/personCategory.ts
+++ src/controller/personCategory.ts
@@ -4,52 +4,56 @@ import { PersonCategory } from "../model/PersonCategory";
 import { Request } from "express";
 import { pc } from "../utils/personCategories";
 import { AppDataSource } from "../data-source";
+import {dbConn} from "../services/db";
 
 class PersonCategoryController extends GenericController<EntityTarget<PersonCategory>> {
   constructor() { super(PersonCategory) }
 
-  async findAllPerCat(request?: Request, CONN?: EntityManager) {
+  async findAllPerCat(req?: Request, CONN?: EntityManager) {
     
     let excludeIds = [pc.ALUN];
-    const userBody = request?.body.user;
+    const userBody = req?.body.user;
+
+    let sqlConnection = await dbConn()
 
     try {
+
+      const qUserTeacher = await this.qTeacherByUser(sqlConnection, req?.body.user.user)
+
       if(!CONN){
-        const uTeacher = await this.teacherByUser(userBody.user)
+        if (!qUserTeacher) { return { status: 404, message: "Usuário não encontrado" } }
+        if (qUserTeacher.person.category.id != userBody.category) { return { status: 403, message: "Usuário não autorizado" } }
 
-        if (!uTeacher) { return { status: 404, message: "Usuário não encontrado" } }
-        if (uTeacher.person.category.id != userBody.category) { return { status: 403, message: "Usuário não autorizado" } }
-
-        if(uTeacher.person.category.id === pc.SUPE){ excludeIds = [...excludeIds, pc.ADMN] }
-        if(uTeacher.person.category.id === pc.FORM){ excludeIds = [...excludeIds, pc.ADMN, pc.SUPE] }
-        if(uTeacher.person.category.id === pc.DIRE){ excludeIds = [...excludeIds, pc.ADMN, pc.SUPE, pc.FORM] }
-        if(uTeacher.person.category.id === pc.VICE){ excludeIds = [...excludeIds, pc.ADMN, pc.SUPE, pc.FORM, pc.DIRE] }
-        if(uTeacher.person.category.id === pc.COOR){ excludeIds = [...excludeIds, pc.ADMN, pc.SUPE, pc.FORM, pc.DIRE, pc.VICE] }
-        if(uTeacher.person.category.id === pc.SECR){ excludeIds = [...excludeIds, pc.ADMN, pc.SUPE, pc.FORM, pc.DIRE, pc.VICE, pc.COOR] }
-        if(uTeacher.person.category.id === pc.MONI){ excludeIds = [...excludeIds, pc.ADMN, pc.SUPE, pc.FORM, pc.DIRE, pc.VICE, pc.COOR, pc.SECR, pc.PROF] }
-        if(uTeacher.person.category.id === pc.PROF){ excludeIds = [...excludeIds, pc.ADMN, pc.SUPE, pc.FORM, pc.DIRE, pc.VICE, pc.COOR, pc.SECR, pc.MONI] }
+        if(qUserTeacher.person.category.id === pc.SUPE){ excludeIds = [...excludeIds, pc.ADMN] }
+        if(qUserTeacher.person.category.id === pc.FORM){ excludeIds = [...excludeIds, pc.ADMN, pc.SUPE] }
+        if(qUserTeacher.person.category.id === pc.DIRE){ excludeIds = [...excludeIds, pc.ADMN, pc.SUPE, pc.FORM] }
+        if(qUserTeacher.person.category.id === pc.VICE){ excludeIds = [...excludeIds, pc.ADMN, pc.SUPE, pc.FORM, pc.DIRE] }
+        if(qUserTeacher.person.category.id === pc.COOR){ excludeIds = [...excludeIds, pc.ADMN, pc.SUPE, pc.FORM, pc.DIRE, pc.VICE] }
+        if(qUserTeacher.person.category.id === pc.SECR){ excludeIds = [...excludeIds, pc.ADMN, pc.SUPE, pc.FORM, pc.DIRE, pc.VICE, pc.COOR] }
+        if(qUserTeacher.person.category.id === pc.MONI){ excludeIds = [...excludeIds, pc.ADMN, pc.SUPE, pc.FORM, pc.DIRE, pc.VICE, pc.COOR, pc.SECR, pc.PROF] }
+        if(qUserTeacher.person.category.id === pc.PROF){ excludeIds = [...excludeIds, pc.ADMN, pc.SUPE, pc.FORM, pc.DIRE, pc.VICE, pc.COOR, pc.SECR, pc.MONI] }
 
         const result = await AppDataSource.getRepository(PersonCategory).createQueryBuilder("personCategory").where("personCategory.id NOT IN (:...ids)", { ids: excludeIds }).getMany();
         return { status: 200, data: result };
       }
 
-      const uTeacher = await this.teacherByUser(userBody.user, CONN)
+      if (!qUserTeacher) { return { status: 404, message: "Usuário não encontrado" } }
+      if (qUserTeacher.person.category.id != userBody.category) { return { status: 403, message: "Usuário não autorizado" } }
 
-      if (!uTeacher) { return { status: 404, message: "Usuário não encontrado" } }
-      if (uTeacher.person.category.id != userBody.category) { return { status: 403, message: "Usuário não autorizado" } }
-
-      if(uTeacher.person.category.id === pc.SUPE){ excludeIds = [...excludeIds, pc.ADMN] }
-      if(uTeacher.person.category.id === pc.FORM){ excludeIds = [...excludeIds, pc.ADMN, pc.SUPE] }
-      if(uTeacher.person.category.id === pc.DIRE){ excludeIds = [...excludeIds, pc.ADMN, pc.SUPE, pc.FORM] }
-      if(uTeacher.person.category.id === pc.VICE){ excludeIds = [...excludeIds, pc.ADMN, pc.SUPE, pc.FORM, pc.DIRE] }
-      if(uTeacher.person.category.id === pc.COOR){ excludeIds = [...excludeIds, pc.ADMN, pc.SUPE, pc.FORM, pc.DIRE, pc.VICE] }
-      if(uTeacher.person.category.id === pc.SECR){ excludeIds = [...excludeIds, pc.ADMN, pc.SUPE, pc.FORM, pc.DIRE, pc.VICE, pc.COOR] }
-      if(uTeacher.person.category.id === pc.MONI){ excludeIds = [...excludeIds, pc.ADMN, pc.SUPE, pc.FORM, pc.DIRE, pc.VICE, pc.COOR, pc.SECR, pc.PROF] }
-      if(uTeacher.person.category.id === pc.PROF){ excludeIds = [...excludeIds, pc.ADMN, pc.SUPE, pc.FORM, pc.DIRE, pc.VICE, pc.COOR, pc.SECR, pc.MONI] }
+      if(qUserTeacher.person.category.id === pc.SUPE){ excludeIds = [...excludeIds, pc.ADMN] }
+      if(qUserTeacher.person.category.id === pc.FORM){ excludeIds = [...excludeIds, pc.ADMN, pc.SUPE] }
+      if(qUserTeacher.person.category.id === pc.DIRE){ excludeIds = [...excludeIds, pc.ADMN, pc.SUPE, pc.FORM] }
+      if(qUserTeacher.person.category.id === pc.VICE){ excludeIds = [...excludeIds, pc.ADMN, pc.SUPE, pc.FORM, pc.DIRE] }
+      if(qUserTeacher.person.category.id === pc.COOR){ excludeIds = [...excludeIds, pc.ADMN, pc.SUPE, pc.FORM, pc.DIRE, pc.VICE] }
+      if(qUserTeacher.person.category.id === pc.SECR){ excludeIds = [...excludeIds, pc.ADMN, pc.SUPE, pc.FORM, pc.DIRE, pc.VICE, pc.COOR] }
+      if(qUserTeacher.person.category.id === pc.MONI){ excludeIds = [...excludeIds, pc.ADMN, pc.SUPE, pc.FORM, pc.DIRE, pc.VICE, pc.COOR, pc.SECR, pc.PROF] }
+      if(qUserTeacher.person.category.id === pc.PROF){ excludeIds = [...excludeIds, pc.ADMN, pc.SUPE, pc.FORM, pc.DIRE, pc.VICE, pc.COOR, pc.SECR, pc.MONI] }
 
       const result = await CONN?.getRepository(PersonCategory).createQueryBuilder("personCategory").where("personCategory.id NOT IN (:...ids)", { ids: excludeIds }).getMany();
       return { status: 200, data: result };
-    } catch (error: any) { return { status: 500, message: error.message } }
+    }
+    catch (error: any) { return { status: 500, message: error.message } }
+    finally { if(sqlConnection) { sqlConnection.release() } }
   }
 }
 

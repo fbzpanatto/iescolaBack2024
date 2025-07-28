75f8e1122be02a326c5346fb6988795fce720251
Fabrizio Panato <fbzpanatto@hotmail.com>
2025-07-28 16:09:36 -0300

    Staging...

--- src/controller/teacherClassDiscipline.ts
+++ src/controller/teacherClassDiscipline.ts
@@ -2,17 +2,24 @@ import { GenericController } from "./genericController";
 import { EntityTarget } from "typeorm";
 import { TeacherClassDiscipline } from "../model/TeacherClassDiscipline";
 import { dbConn } from "../services/db";
+import {UserInterface} from "../interfaces/interfaces";
 
 class TeacherClassDisciplineController extends GenericController<EntityTarget<TeacherClassDiscipline>> {
   constructor() { super(TeacherClassDiscipline) }
 
-  async updateContract(body: { teacherId: number, schoolId: number, contractId: number, categoryId: number, yearId: number, yearName: string, classroom: number }){
+  async updateContract(body: { teacherId: number, schoolId: number, contractId: number, categoryId: number, yearId: number, yearName: string, classroom: number, user: UserInterface }){
 
     let conn = await dbConn();
 
     try {
 
       await conn.beginTransaction();
+
+      const teacher = await this.qTeacherByUser(conn, body.user.user);
+
+      if(![1, 2, 10].includes(teacher.person.category.id)){
+        return { status: 403, message: 'Você não tem permissão para acessar ou modificar este recurso.' }
+      }
 
       const currentYear = await this.qCurrentYear(conn)
 

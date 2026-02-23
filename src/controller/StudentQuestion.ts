ee1aeb2c2a16df2564e56eb09361d0d1a05eca15
Fabrizio Panato <fbzpanatto@hotmail.com>
2026-02-23 10:33:45 -0300

    updateReadingFluency refactoring to use connectionPool

--- src/controller/StudentQuestion.ts
+++ src/controller/StudentQuestion.ts
@@ -12,12 +12,13 @@ import { Student} from "../model/Student";
 import { Classroom } from "../model/Classroom";
 import { UserInterface } from "../interfaces/interfaces";
 import { StudentClassroom } from "../model/StudentClassroom";
+import { connectionPool } from "../services/db";
 
 class StudentQuestionController extends GenericController<EntityTarget<StudentQuestion>> {
 
   constructor() { super(StudentQuestion)}
 
-  async updateReadingFluency(req: Request){
+  async updateReadingFluency_backUp(req: Request){
 
     const { body, query } = req
     const { year } = query
@@ -63,6 +64,107 @@ class StudentQuestionController extends GenericController<EntityTarget<StudentQu
       })
     }
     catch (error: any) { console.log('error', error); return { status: 500, message: error.message } }
+  }
+
+  async updateReadingFluency(req: Request) {
+
+    const { body, query } = req;
+    const { year } = query;
+    let conn;
+
+    try {
+      conn = await connectionPool.getConnection();
+      await conn.beginTransaction();
+
+      const qUserTeacher = await this.qTeacherByUser(body.user.user);
+      const userId = qUserTeacher.person.user.id;
+
+      const cY = await this.qCurrentYear();
+      if (!cY) { await conn.rollback(); return { status: 400, message: 'Ano não encontrado' } }
+      if (parseInt(cY.name) !== parseInt(year as string)) { await conn.rollback(); return { status: 400, message: 'Não é permitido alterar o gabarito de anos anteriores.' }}
+
+      const [testRows] = await conn.query(`SELECT active FROM test WHERE id = ?`, [body.test.id]) as Array<any>;
+
+      if (testRows.length > 0 && !testRows[0].active) { await conn.rollback(); return { status: 403, message: 'Essa avaliação não permite novos lançamentos.' } }
+
+      const [rfRows] = await conn.query(
+        `SELECT id, readingFluencyLevelId, rClassroomId 
+       FROM reading_fluency 
+       WHERE testId = ? AND readingFluencyExamId = ? AND studentId = ?`,
+        [body.test.id, body.readingFluencyExam.id, body.student.id]
+      );
+
+      const readingFluency = (rfRows as any[])[0];
+
+      if (!readingFluency) {
+        const insertQuery =
+          `
+            INSERT INTO reading_fluency 
+            (testId, readingFluencyExamId, studentId, rClassroomId, readingFluencyLevelId, createdByUser, createdAt) 
+            VALUES (?, ?, ?, ?, ?, ?, NOW())
+          `;
+
+        const levelId = body.readingFluencyLevel?.id || null;
+
+        const [insertResult]: any = await conn.query(insertQuery, [body.test.id, body.readingFluencyExam.id, body.student.id, body.classroom.id, levelId, userId]);
+
+        await conn.commit();
+
+        return { status: 201, data: { id: insertResult.insertId, ...body } };
+      }
+
+      const scQuery = `
+      SELECT 
+        sc.endedAt,   
+        p.name AS studentName, 
+        c.shortName AS classroomName, 
+        s.shortName AS schoolName
+        FROM student_classroom sc
+        INNER JOIN student st ON sc.studentId = st.id
+        INNER JOIN person p ON st.personId = p.id
+        INNER JOIN classroom c ON sc.classroomId = c.id
+        INNER JOIN school s ON c.schoolId = s.id
+        WHERE sc.id = ?
+      `;
+
+      const [scRows] = await conn.query(scQuery, [body.studentClassroom.id]);
+      const sC = (scRows as any[])[0];
+
+      if (sC?.endedAt && !readingFluency.readingFluencyLevelId) {
+        await conn.rollback();
+        return { status: 403, message: `${sC.studentName} consta como matrícula encerrada para ${sC.classroomName} - ${sC.schoolName}.` };
+      }
+
+      if (readingFluency.readingFluencyLevelId && readingFluency.rClassroomId && readingFluency.rClassroomId !== body.classroom.id) {
+        const messageErr1 = 'Você não pode alterar um nível de alfabetização que já foi registrado em outra sala/escola.';
+        await conn.rollback(); return { status: 403, message: messageErr1 }
+      }
+
+      const updateQuery = `
+        UPDATE reading_fluency 
+        SET readingFluencyLevelId = ?, rClassroomId = ?, updatedByUser = ?, updatedAt = NOW() 
+        WHERE id = ?
+      `;
+
+      const newLevelId = body.readingFluencyLevel?.id || null;
+      const newClassroomId = newLevelId ? body.classroom.id : null;
+
+      await conn.query(updateQuery, [ newLevelId, newClassroomId, userId, readingFluency.id]);
+
+      await conn.commit();
+
+      return {
+        status: 201,
+        data: {
+          ...body,
+          id: readingFluency.id,
+          readingFluencyLevel: newLevelId ? body.readingFluencyLevel : null,
+          rClassroom: newClassroomId ? body.classroom : null
+        }
+      };
+    }
+    catch (error: any) { if (conn) { await conn.rollback() } console.log('error', error); return { status: 500, message: error.message } }
+    finally { if (conn) { conn.release() } }
   }
 
   async updateAlphabetic(req: Request){

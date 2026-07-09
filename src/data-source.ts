46903ec1b198034bf84917bc951921a720b9e596
Fabrizio Panato <fbzpanatto@hotmail.com>
2026-07-09 13:34:57 -0300

    synchronize: false,

--- src/data-source.ts
+++ src/data-source.ts
@@ -10,7 +10,7 @@ export const AppDataSource = new DataSource({
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  synchronize: true,
+  synchronize: false,
   logging: false,
   entities: entities,
   connectTimeout: 60000
